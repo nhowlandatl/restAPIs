bb0deb1463c2e8f1ac0d9d2e1b7f1b077ce59314
Nick Howland <nhowland@outlook.com>
2020-04-09 12:49:53 -0400

    trying to get put request to work

--- server.js
+++ server.js
@@ -13,14 +13,53 @@ var todoList = [
 ];
 
 // GET /api/todos
+app.get('/api/todos', (req, res, next) => 
+    res.send(todoList))
 
 // GET /api/todos/:id
+app.get('/api/todos:id', function(req, res, next) {
+    let id = req.params.id
+    let foundID = todolist.find(function(item) {
+        return item.id == id;
+    })
+    res.send(foundID);
+})
 
 // POST /api/todos
+app.post('/api/todos', function(req, res, next) {
+    let nextId = todoList.reduce((acc,element) =>{
+        if(element.id > acc) {
+            return element.id
+        }
+        return acc++
+    }, 0) + 1
+    let toDoObj = {
+        id: nextId,
+        todo: req.body.todo
+    }
+    todoList.push(toDoObj);
+    res.send(toDoObj);
+})
 
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (req, res, next) => {
+        let updatedTodo = todoList.find(function (todo) {
+            if(todo.id) {
+                return todo.id === parseInt(req.params.id);
+            }
+        });
 
+        let updated = {
+            id: updatedTodo,
+            todo: req.body.todo
+        };
+
+        todoList.push(updated);     
+        res.send(updated);
+})
 // DELETE /api/todos/:id
+
+
 
 app.listen(3000, function(){
     console.log('Todo List API is now listening on port 3000...');
